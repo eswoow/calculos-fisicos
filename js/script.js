a0ee16ceb2d5290df8e4f944f3351f87c362eab5
eswo <164808454+eswoow@users.noreply.github.com>
2025-09-30 17:05:07 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@ function velocidadeMedia(){
     let desloc = document.getElementById("desloc").value;
     let tempo = document.getElementById("tempo").value;
     let resulVelocMedia = document.getElementById("resulVelocMedia");
-
     let velMedia = desloc / tempo;
+    if (tempo <= 0){
+        resulVelocMedia.innerHTML = `O tempo não pode ser menor ou igual a zero.`;
+        return;
+    }
     resulVelocMedia.innerHTML = `Resultado: ${velMedia} m/s`;
 }
 function forcaResultante(){
     let massa = document.getElementById("massa").value;
     let aceleracao = document.getElementById("acel").value;
     let resulForca = document.getElementById("resulForca");
-
     let forca = massa * aceleracao;
+    if (massa < 0){
+        resulForca.innerHTML = `A massa não pode ser negativa.`;
+        return;
+    }
     resulForca.innerHTML = `Resultado: ${forca} N`;
 }
 function energiaCinetica(){
@@ -19,6 +25,9 @@ function energiaCinetica(){
     let velocCin = document.getElementById("velocidadeCin").value;
     let resulCin = document.getElementById("resulEnergiaCin");
     let energia = 0.5*massaCin*(velocCin*velocCin);
+    if (massaCin < 0){
+        resulCin.innerHTML = `A massa não pode ser negativa.`;
+    }
     resulCin.innerHTML = `Resultado: ${energia} J`;
 }
 function potenciaMecanica(){
@@ -26,6 +35,9 @@ function potenciaMecanica(){
     let tempoPot = document.getElementById("tempoPot").value;
     let resulPot = document.getElementById("resulPotencia");
     let potencia = energiaPot/tempoPot;
+    if (tempoPot <= 0){
+        resulPot.innerHTML = `O tempo não pode ser menor ou igual a zero.`;
+    }
     resulPot.innerHTML = `Resultado: ${potencia} W`;
 }
 function pressao(){
@@ -33,6 +45,8 @@ function pressao(){
     let areaPress = document.getElementById("areaPress").value;
     let resulPress = document.getElementById("resulPressao");
     let pressao = forcaPress/areaPress;
+    if (areaPress <= 0){
+        resulPress.innerHTML = `A área não pode ser menor ou igual a zero.`;
+    }
     resulPress.innerHTML = `Resultado: ${pressao} Pa`;
-    
 }
